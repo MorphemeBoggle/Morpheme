5478404a23dc03f309fa66b34ebc62a84dcac613
Gewurztraminer <Gewurztraminer@GewurztinersMBP.hitronhub.home>
2016-06-03 01:39:14 -0500

    “osidjfiosjf”

--- Boggle/src/Morpheme/test.java
+++ Boggle/src/Morpheme/test.java
@@ -2,43 +2,4 @@ package Morpheme;
 
 public class test {
 	
-	public static String[] brackets = { "()", "{}", "[]" };
-
-
-	public static void main(String[] args){
-		
-		valid_braces("(()())")
-		
-		
-	}
-	
-	
-	public static boolean valid_braces(String braces){
-		
-	}
-	
-	public static boolean isForwardBracket(String s){
-		
-		boolean torf = false;
-		
-		if (s.equals("(") || s.equals("{") || s.equals("[")){
-			torf = true;
-		}
-		
-		return torf;
-	}
-	
-	public static String getReverse(String s){
-		
-		String reverse = "";
-		
-		for (int h = 0; h < brackets.length; h++){
-			if (s.equals(brackets[h].substring(0, 1))){
-				reverse = brackets[h].substring(1, 2);
-			}
-		}
-		
-		return reverse;
-	}
-	
 }
